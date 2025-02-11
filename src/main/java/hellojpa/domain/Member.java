07de4b4d426feb48484ca2a63c0c1c50400258d9
baltol96 <baltol@naver.com>
2025-02-11 23:59:36 +0900

    다양한 연관관계 매핑
    - 다대일[N:1] : @ManyToOne
    가장 많이 사용하는 연관관계
    우선적으로 사용
    
    - 일대다[1:N] : @OneToMany
    테이블에는 항상 다(N)에 외래키가 있지만 일대다 단방향은 일(1)이 연관관계 주인
    엔티티와 테이블의 외래키의 위치가 서로 다름
    
    - 일대일[1:1] : @OneToOne
    외래키가 있는 곳이 연관관계 주인
    반대편은 mappedMy 적용
    단방향 관계는 JPA 지원X
    
    - 다대다[N:M] : @ManyToMany
    실무에서 사용X

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -15,7 +15,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     private String city;
     private String street;
     private String zipcode;
